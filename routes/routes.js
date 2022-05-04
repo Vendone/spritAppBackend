035da84ec44f4ef76e159db27adff7da04daa95d
Vendone <andreasventurin@gmail.com>
2022-05-04 19:50:31 +0200

    Post request funktioniert

--- routes/routes.js
+++ routes/routes.js
@@ -17,12 +17,12 @@ routesRouter.get('/', (req, res, next) => {
 
 routesRouter.post('/', (req, res, next) => {
 
-    const { date, start_point, end_point, mileage_start, mileage_stop, avg_fuel_consumption, user_id, car_id } = req.body;
-    pool.query('INSERT INTO routes (date, start_point, end_point, mileage_start, mileage_stop, avg_fuel_consumption, user_id, car_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [date, start_point, end_point, mileage_start, mileage_stop, avg_fuel_consumption, user_id, car_id], (err, results) => {
+    const { start_point, end_point, mileage_start, mileage_stop, avg_fuel_consumption, user_id, car_id } = req.body;
+    pool.query('INSERT INTO routes (start_point, end_point, mileage_start, mileage_stop, avg_fuel_consumption, user_id, car_id) VALUES ($1, $2, $3, $4, $5, $6, $7)', [start_point, end_point, mileage_start, mileage_stop, avg_fuel_consumption, user_id, car_id], (err, results) => {
         if (err) {
             next(err);
         } else {
-            res.status(201);
+            res.status(201).send({ ok: 'ok' });
         }
     })
 })
