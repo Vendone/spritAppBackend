842eb9549da6aaba413d92774a222f0a5233f334
Vendone <andreasventurin@gmail.com>
2022-05-29 16:10:14 +0200

    redirect geändert

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ function checkAuthenticated(req, res, next) {
 
 function checkNotAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
-        return res.redirect('/');
+        return res.redirect('http://localhost:3000');
     }
     next();
 }
