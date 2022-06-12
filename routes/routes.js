e8755667971fd84f64a1bd279c053628d680988c
Vendone <andreasventurin@gmail.com>
2022-06-12 17:09:58 +0200

    routes umgestellt

--- routes/routes.js
+++ routes/routes.js
@@ -1,62 +1,29 @@
 const express = require('express');
 const routesRouter = express.Router();
-const { pool } = require('../dbConfig');
+const { getData, postData, putById, deleteById } = require('../dbquerys');
 
 routesRouter.get('/', async (req, res, next) => {
-    try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT * FROM routes WHERE user_id = $1', [1]);
-        const results = { 'results': (result) ? result.rows : null };
-        res.status(200).send(results);
-        client.release();
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    }
+    const response = await getData('routes');
+    res.status(200).send(response);
 })
 
 routesRouter.post('/', async (req, res, next) => {
     const { start_point, end_point, mileage_start, mileage_stop, avg_fuel_consumption, user_id, car_id } = req.body;
-    try {
-        const client = await pool.connect();
-        const result = await client.query('INSERT INTO routes (start_point, end_point, mileage_start, mileage_stop, avg_fuel_consumption, user_id, car_id) VALUES ($1, $2, $3, $4, $5, $6, $7)',
-            [start_point, end_point, mileage_start, mileage_stop, avg_fuel_consumption, user_id, car_id]);
-        res.status(201).send({ ok: 'ok' });
-        client.release();
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    }
+    const response = await postData('routes', 'start_point, end_point, mileage_start, mileage_stop, avg_fuel_consumption, user_id, car_id', [start_point, end_point, mileage_start, mileage_stop, avg_fuel_consumption, user_id, car_id]);
+    res.status(200).send(response);
 })
 
-routesRouter.put('/:id', (req, res, next) => {
+routesRouter.put('/:id', async (req, res, next) => {
     const { start_point, end_point, mileage_start, mileage_stop, avg_fuel_consumption, car_id } = req.body;
-    pool.query('UPDATE routes SET start_point = $1, end_point = $2, mileage_start = $3, mileage_stop = $4, avg_fuel_consumption= $5, car_id= $6 WHERE id = $7',
-        [start_point, end_point, mileage_start, mileage_stop, avg_fuel_consumption, car_id, req.params.id],
-        (err, results) => {
-            if (err) {
-                next(err);
-            } else {
-                res.status(201).send({ ok: results });
-            }
-        });
+    const id = req.params.id;
+    const response = await putById('routes', ['start_point', 'end_point', 'mileage_start', 'mileage_stop', 'avg_fuel_consumption', 'car_id'], [start_point, end_point, mileage_start, mileage_stop, avg_fuel_consumption, car_id, req.params.id]);
+    res.status(201).send(response);
 })
 
-routesRouter.delete('/:id', (req, res, next) => {
-    pool.query('DELETE FROM routes WHERE id=$1', [req.params.id], (err, results) => {
-        if (err) {
-            next(err);
-        } else {
-            let routes = results.rows;
-            const route = routes;
-            res.status(200).send(route);
-        }
-    });
-})
-
-routesRouter.get('/test', (req, res, next) => {
-    const user = req.session.passport.user.id;
-    res.status(200).send({ user });
+routesRouter.delete('/:id', async (req, res, next) => {
+    const id = req.params.id;
+    const response = await deleteById('routes', id);
+    res.status(200).send(response);
 })
 
 module.exports = routesRouter;
