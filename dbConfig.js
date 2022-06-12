c40c8ec6f05dabdaab9e9a4463d84750b29b5cf0
Vendone <andreasventurin@gmail.com>
2022-06-12 09:16:59 +0200

    ssl serverabfrage für production hinzugefügt

--- dbConfig.js
+++ dbConfig.js
@@ -4,10 +4,10 @@ const { Pool } = require('pg');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-
-const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString
-});
+const connectionString = { connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}` };
+const productionSsl = { connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } };
+const pool = new Pool(
+    isProduction ? productionSsl : connectionString
+);
 
 module.exports = { pool };
