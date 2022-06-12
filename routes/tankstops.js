70ca0c4cf55790c4e6cd622dac7f461679d7cc11
Vendone <andreasventurin@gmail.com>
2022-06-12 17:16:10 +0200

    tankstops hinzhugefügt

--- routes/tankstops.js
+++ routes/tankstops.js
@@ -1,53 +1,29 @@
 const express = require('express');
 const tankstopsRouter = express.Router();
-const { pool } = require('../dbConfig');
+const { getData, postData, putById, deleteById } = require('../dbquerys');
 
-tankstopsRouter.get('/', (req, res, next) => {
-    pool.query('SELECT * FROM tank_stops WHERE user_id = 1', (err, results) => {
-        if (err) {
-            next(err);
-        } else {
-            let tankstops = results.rows;
-            const tankstop = tankstops;
-            res.status(200).send(tankstop);
-        }
-    });
+tankstopsRouter.get('/', async (req, res, next) => {
+    const response = await getData('tank_stops');
+    res.status(200).send(response);
 })
 
-tankstopsRouter.post('/', (req, res, next) => {
+tankstopsRouter.post('/', async (req, res, next) => {
     const { gasstation_id, fuel, amount, price, milage, date, user_id, car_id } = req.body;
-    pool.query('INSERT INTO tank_stops (gasstation_id, fuel, amount, price, milage, date, user_id, car_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
-        [gasstation_id, fuel, amount, price, milage, date, user_id, car_id],
-        (err, results) => {
-            if (err) {
-                next(err);
-            } else {
-                res.status(201).send('ok');
-            }
-        })
+    const response = await postData('tank_stops', 'gasstation_id, fuel, amount, price, milage, date, user_id, car_id', [gasstation_id, fuel, amount, price, milage, date, user_id, car_id]);
+    res.status(200).send(response);
 })
 
-tankstopsRouter.put('/:id', (req, res, next) => {
+tankstopsRouter.put('/:id', async (req, res, next) => {
     const { gasstation_id, fuel, amount, price, milage, date, user_id, car_id } = req.body;
-    pool.query('UPDATE tank_stops SET gasstation_id = $1, fuel = $2, amount = $3, price = $4, milage = $5, date = $6, user_id = $7, car_id = $8 WHERE id = $9',
-        [gasstation_id, fuel, amount, price, milage, date, user_id, car_id, req.params.id],
-        (err, results) => {
-            if (err) {
-                next(err);
-            } else {
-                res.status(201).send({ ok: 'ok' });
-            }
-        });
+    const id = req.params.id;
+    const response = await putById('tank_stops', ['gasstation_id', 'fuel', 'amount', 'price', 'milage', 'date', 'user_id', 'car_id'], [gasstation_id, fuel, amount, price, milage, date, user_id, car_id, req.params.id]);
+    res.status(201).send(response);
 })
 
-tankstopsRouter.delete('/:id', (req, res, next) => {
-    pool.query('DELETE FROM tank_stops WHERE id=$1', [req.params.id], (err, results) => {
-        if (err) {
-            next(err);
-        } else {
-            res.status(200).send({ ok: 'ok' });
-        }
-    });
+tankstopsRouter.delete('/:id', async (req, res, next) => {
+    const id = req.params.id;
+    const response = await deleteById('tank_stops', id);
+    res.status(200).send(response);
 })
 
 module.exports = tankstopsRouter;
