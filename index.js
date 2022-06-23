61a42397275b40b031b3cf91c3eb09980e0e0616
Vendone <andreasventurin@gmail.com>
2022-06-23 07:20:41 +0200

    helmet eingebaut

--- index.js
+++ index.js
@@ -4,30 +4,31 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express');
 const app = express();
-const cors = require('cors');
+const PORT = process.env.PORT;
 const path = require("path");
-app.use(express.static(path.join(__dirname, 'public')));
 const flash = require('express-flash');
-app.set('view-engine', 'ejs');
-app.use(express.urlencoded({ extended: false }))
-app.use(flash());
 const logger = require('morgan');
-app.use(logger('dev'));
-app.use(express.json());
-const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header');
-app.use(cors({
+const { pool } = require('./dbConfig');
+const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
+const SESSION_SECRET = process.env.SESSION_SECRET;
+const helmet = require('helmet');
+//const cors = require('cors');
+//const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header');
+/*app.use(cors({
     "origin": "*",
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "preflightContinue": false,
     "optionsSuccessStatus": 204
-}));
-const { pool } = require('./dbConfig');
-const PORT = process.env.PORT;
+}));*/
 
-const session = require('express-session');
-const pgSession = require('connect-pg-simple')(session);
-const SESSION_SECRET = process.env.SESSION_SECRET;
-
+app.set('view-engine', 'ejs');
+app.use(helmet());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }))
+app.use(flash());
+app.use(logger('dev'));
+app.use(express.json());
 app.use(session({
     secret: SESSION_SECRET,
     store: new pgSession({
@@ -39,10 +40,10 @@ app.use(session({
     saveUninitialized: false,
     sameSite: 'none',
     secure: true,
-    cookie: { path: '/', httpOnly: false, secure: true, sameSite: 'none', maxAge: 100 * 60 * 60 * 24 }
+    cookie: { path: '/', httpsOnly: true, secure: true, sameSite: 'none', maxAge: 100 * 60 * 60 * 24 }
 }));
 
-app.use(expressCspHeader({
+/*app.use(expressCspHeader({
     directives: {
         'default-src': [SELF, 'https://ka-f.fontawesome.com/', 'https://fonts.gstatic.com/'],
         'script-src': [SELF, INLINE, process.env.FRONTEND_URL, 'https://kit.fontawesome.com'],
@@ -51,7 +52,7 @@ app.use(expressCspHeader({
         'worker-src': [NONE],
         'block-all-mixed-content': false
     }
-}));
+}));*/
 
 //Routes
 const routesRouter = require('./routes/routes');
@@ -71,19 +72,6 @@ app.use('/user', usersRouter)
 app.use('/gasstations', gasstationRouter)
 app.use('/tankstops', tankstopsRouter)
 app.use('/auth', authLocal)
-
-app.get('/test', async (req, res, next) => {
-    try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT * FROM gasstations');
-        const results = { 'results': (result) ? result.rows : null };
-        res.status(200).send(results);
-        client.release();
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    }
-})
 
 //Error handling
 app.use(function (err, req, res, next) {
