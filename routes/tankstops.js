994a30f6856dfd1ff74b0c073bd6d9b3da2802ba
Vendone <andreasventurin@gmail.com>
2022-06-24 07:38:23 +0200

    user input

--- routes/tankstops.js
+++ routes/tankstops.js
@@ -1,19 +1,46 @@
 const express = require('express');
 const tankstopsRouter = express.Router();
 const { getData, postData, putById, deleteById } = require('../dbquerys');
+const { check, validationResult } = require('express-validator');
 
 tankstopsRouter.get('/', async (req, res, next) => {
     const response = await getData('tank_stops');
     res.status(200).send(response);
 })
 
-tankstopsRouter.post('/', async (req, res, next) => {
+tankstopsRouter.post('/', [
+    check('gasstation_id').isNumeric().blacklist('<>"/;'),
+    check('fuel').notEmpty().isString().blacklist('<>"/;'),
+    check('amount').isNumeric().blacklist('<>"/;'),
+    check('price').isNumeric().blacklist('<>"/;'),
+    check('milage').isNumeric().blacklist('<>"/;'),
+    check('date').isISO8601().toDate(),
+    check('user_id').isNumeric().blacklist('<>"/;'),
+    check('car_id').isNumeric().blacklist('<>"/;')
+], async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
     const { gasstation_id, fuel, amount, price, milage, date, user_id, car_id } = req.body;
     const response = await postData('tank_stops', 'gasstation_id, fuel, amount, price, milage, date, user_id, car_id', [gasstation_id, fuel, amount, price, milage, date, user_id, car_id]);
     res.status(200).send(response);
 })
 
-tankstopsRouter.put('/:id', async (req, res, next) => {
+tankstopsRouter.put('/:id', [
+    check('gasstation_id').isNumeric().blacklist('<>"/;'),
+    check('fuel').notEmpty().isString().blacklist('<>"/;'),
+    check('amount').isNumeric().blacklist('<>"/;'),
+    check('price').isNumeric().blacklist('<>"/;'),
+    check('milage').isNumeric().blacklist('<>"/;'),
+    check('date').isISO8601().toDate(),
+    check('user_id').isNumeric().blacklist('<>"/;'),
+    check('car_id').isNumeric().blacklist('<>"/;')
+], async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
     const { gasstation_id, fuel, amount, price, milage, date, user_id, car_id } = req.body;
     const id = req.params.id;
     const response = await putById('tank_stops', ['gasstation_id', 'fuel', 'amount', 'price', 'milage', 'date', 'user_id', 'car_id'], [gasstation_id, fuel, amount, price, milage, date, user_id, car_id, req.params.id]);
